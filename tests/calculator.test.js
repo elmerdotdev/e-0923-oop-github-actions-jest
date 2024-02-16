ae176336d9f851878d8b8ef40bbc5e181315a86c
Elmer Balbin <contact@elmer.dev>
2024-02-15 18:35:47 -0800

    first commit

---
+++ tests/calculator.test.js
@@ -0,0 +1,21 @@
+const Calculator = require('../calculator')
+
+test('1 plus 2 equals 3', () => {
+  const calc = new Calculator()
+  expect(calc.add(1, 2)).toBe(3)
+})
+
+test('10 minus 5 equals 5', () => {
+  const calc = new Calculator()
+  expect(calc.sub(10, 5)).toBe(5)
+})
+
+test('3 multiply by 3 equals 9', () => {
+  const calc = new Calculator()
+  expect(calc.mul(3, 3)).toBe(9)
+})
+
+test('100 divided by 10 equals 10', () => {
+  const calc = new Calculator()
+  expect(calc.div(100, 10)).toBe(10)
+})
